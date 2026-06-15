d01281b6dde398247ee5264e3f772f99bdbd0e27
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-15 17:18:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ st.write('Comment était la course?')
 myCont = st.container(horizontal=True, horizontal_alignment="center")
 with myCont:
   if st.button(f':+1:'):
-    st.write('Super!')
+    #st.write('Super!')
     test=True
   if st.button(f':-1:'):
-    st.write('Bof')
+    #st.write('Bof')
     test=False
 with st.container(horizontal=True, horizontal_alignment="center"):
     'Super' if test else 'bof'

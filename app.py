1586a3030c60f78183149b618dbc8b73b7bcce5c
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-15 16:34:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
+answer=""
+
 if st.button(f'Coucou 🚗'):
   st.write("Comment était la course?")
   myCont = st.container(horizontal=True, horizontal_alignment="center")
-  answer=""
   with myCont:
     if st.button(f':+1:'):
       answer="Super!"

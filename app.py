cf16460576ad4c2f03ade34480c0fe159d2bd6d7
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-15 16:21:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
 if st.button(f'Coucou 🚗'):
-  st.write("Vroum")
+  st.write("Comment était la course?"):
+  if st.button(f'	:+1: ')
+  st.write("Super")
+elif st.write("Bof")
+  

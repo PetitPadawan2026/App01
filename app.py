46533c879faef0a22c17d736b1607cb50e78f9aa
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-17 16:38:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -88,4 +88,7 @@ pages = {
 pg = st.navigation(pages)
 pg.run()
 
-
+sentiment_mapping = ["one", "two", "three", "four", "five"]
+selected = st.feedback("stars")
+if selected is not None:
+    st.markdown(f"You selected {sentiment_mapping[selected]} star(s)."

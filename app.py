a5ada2d64ec9fd1116757a092ec5a6541e1a2ace
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-17 16:41:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -91,4 +91,5 @@ pg.run()
 sentiment_mapping = ["one", "two", "three", "four", "five"]
 selected = st.feedback("stars")
 if selected is not None:
-    st.markdown(f"You selected {sentiment_mapping[selected]} star(s)."
+    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
+               

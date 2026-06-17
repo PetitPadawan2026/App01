c50804989bbf9a84685ddf3863160464db75af39
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-17 17:14:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -103,3 +103,27 @@ option = st.selectbox(
     placeholder="Select contact method...",
 )
 st.write("You selected:", option)
+
+
+
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
+    st.session_state.horizontal = False
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.checkbox("Disable radio widget", key="disabled")
+    st.checkbox("Orient radio options horizontally", key="horizontal")
+
+with col2:
+    st.radio(
+        "Set label visibility 👇",
+        ["visible", "hidden", "collapsed"],
+        key="visibility",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        horizontal=st.session_state.horizontal,
+    )

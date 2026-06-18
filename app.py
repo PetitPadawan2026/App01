bb612e2a3a89ad01763bc1a00f2cbed98ad72adc
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-18 16:47:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ pages = {
  "Resources": [
   st.Page(page_1, title="Page 1", icon="📰"),
   st.Page(page_2, title="Page 2", icon="📰"),
-  st.Page(page_take_rdv, title="Prendre rendez-vous", icon="📋"),
   st.Page("calendar.py", title="Calendrier", icon="📋"),
   st.Page("form.py", title="Formulaires", icon="📋"),
  ],

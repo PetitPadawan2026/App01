218eb87b45e0009922cc002c45c996f7ad680288
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-18 16:45:55 +0200

    Update game.py

--- game.py
+++ game.py
@@ -3,6 +3,21 @@ import pandas as pd
 from numpy.random import default_rng as rng
 
 
+def page1_cont():
+  st.write('Comment était la course?')
+  myCont = st.container(horizontal=True, horizontal_alignment="center")
+  test=False
+  with myCont:
+    if st.button(f':+1:'):
+      #st.write('Super!')  
+      test=True
+    if st.button(f':-1:'):
+      #st.write('Bof')
+      test=False
+  with st.container(horizontal=True, horizontal_alignment="center"):
+      'Super' if test else 'bof'
+
+
 
 color = build_slider("Rate the Race",notes)
   st.write("My favorite number is", color)  

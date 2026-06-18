775de31593928d7899c5fc328d86582ff6c64b46
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-18 16:24:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -61,11 +61,6 @@ pg.run()
 
 
 
-sentiment_mapping = ["one", "two", "three", "four", "five"]
-selected = st.feedback("stars")
-if selected is not None:
-    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
-    st.write("Merci pour votre réponse") 
 
 
  #Sélectionner un niveau pour l'élève

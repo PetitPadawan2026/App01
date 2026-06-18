c857d382f1fabb1f6fa9e270a1a4c85b87372676
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-18 14:50:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,31 +26,6 @@ def build_slider(label="Demo",values=[]):
 
 
 def page2_cont():
-  color = build_slider("Rate the Race",notes)
-  st.write("My favorite number is", color)  
-
-  cars = [":red [Ferrari]", "Mclaren", "Porsche", "Aston Martin","Alpine", "Cadillac" , "Peugeot" , "Toyota" ]
-  action = st.menu_button("Quel est ta voiture préféré", options=cars)
-  try:
-    if action is not None:
-      st.write(f"Ta voiture préféré est {action}")
-  except:
-    pass
-
-  demo = build_slider("Vroums",cars)
-  tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs(cars)
-  agree = st.checkbox("I agree")
-  genre = st.radio(
-      ":rainbow[Are you sure ?]",
-      ["I'm sure", "I'm not sure"],
-    )
-    
-  if genre == "I'm sure":
-    st.write("Great!")
-  else:
-    st.write("Please select", "I'm sure")
-    st.snow()
-
 
 def page_1():
  st.write("***Page 1***")

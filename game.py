39c53830bf97dc28ded00e5ac4a5a08830aefb43
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-18 16:28:41 +0200

    Update game.py

--- game.py
+++ game.py
@@ -61,3 +61,26 @@ option = st.selectbox(
     placeholder="Select contact method...",
 )
 st.write("You selected:", option)
+
+
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
+    st.session_state.horizontal = False
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.checkbox("Je n'accepte pas", key="disabled")
+    st.checkbox("J'accepte")
+
+with col2:
+    st.radio(
+        "Votre nage préférée 👇",
+        ["Brasse", "Crawl", "Papillon"],
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        horizontal=st.session_state.horizontal,
+    ) 
+

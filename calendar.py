f0ea59280812e901da97c48e9f3b9f093aa1df87
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-19 15:35:06 +0200

    Test

--- calendar.py
+++ calendar.py
@@ -25,7 +25,8 @@ calendar_options = {
 calendar_events = [
     { "title": "Event 1", "start": "2026-06-16T08:30:00", "end": "2026-06-16T10:30:00", "resourceId": "a", },
     { "title": "Event 2", "start": "2026-06-17T07:30:00", "end": "2026-06-17T10:30:00", "resourceId": "b", },
-    { "title": "Event 3", "start": "2026-06-18T10:40:00", "end": "2026-06-19T12:30:00", "resourceId": "c", }
+    { "title": "Event 3", "start": "2026-06-18T10:40:00", "end": "2026-06-19T12:30:00", "resourceId": "c", },
+    { "title": "Event 4", "start": "2026-06-18T10:40:00", "end": "2026-06-19T12:30:00", "resourceId": "a", }
 ]
 custom_css="""
     .fc-event-past {

fdf539051a7cc99f38aa5c98fbfbc52c2ae047a2
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-19 11:42:24 +0200

    Update inc_streamlit_app.py

--- inc_streamlit_app.py
+++ inc_streamlit_app.py
@@ -3,21 +3,28 @@ import streamlit as st
 if "role" not in st.session_state:
     st.session_state.role = None
 
-role = st.session_state.role
 ROLES = [None, "Requester", "Responder", "Admin"]
 
+
 def login():
+
     st.header("Log in")
     role = st.selectbox("Choose your role", ROLES)
 
-def logout():
-  st.session_state.role = None
-  st.rerun()
+    if st.button("Log in"):
+        st.session_state.role = role
+        st.rerun()
 
+
+def logout():
+    st.session_state.role = None
+    st.rerun()
+
+
+role = st.session_state.role
 
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 settings = st.Page("settings.py", title="Settings", icon=":material/settings:")
-
 request_1 = st.Page(
     "inc_request_1.py",
     title="Request 1",
@@ -27,7 +34,6 @@ request_1 = st.Page(
 request_2 = st.Page(
     "inc_request_2.py", title="Request 2", icon=":material/bug_report:"
 )
-
 respond_1 = st.Page(
     "inc_respond_1.py",
     title="Respond 1",
@@ -37,7 +43,6 @@ respond_1 = st.Page(
 respond_2 = st.Page(
     "inc_respond_2.py", title="Respond 2", icon=":material/handyman:"
 )
-
 admin_1 = st.Page(
     "inc_admin_1.py",
     title="Admin 1",
@@ -46,20 +51,15 @@ admin_1 = st.Page(
 )
 admin_2 = st.Page("inc_admin_2.py", title="Admin 2", icon=":material/security:")
 
-
 account_pages = [logout_page, settings]
 request_pages = [request_1, request_2]
 respond_pages = [respond_1, respond_2]
 admin_pages = [admin_1, admin_2]
 
-
-
-if st.button("Log in"):
-    st.session_state.role = role
-    st.rerun()
+st.title("Request manager")
+#st.logo("images/horizontal_blue.png", icon_image="images/icon_blue.png")
 
 page_dict = {}
-st.write(st.session_state.role,role)
 if st.session_state.role in ["Requester", "Admin"]:
     page_dict["Request"] = request_pages
 if st.session_state.role in ["Responder", "Admin"]:
@@ -69,10 +69,7 @@ if st.session_state.role == "Admin":
 
 if len(page_dict) > 0:
     pg = st.navigation({"Account": account_pages} | page_dict)
-
 else:
     pg = st.navigation([st.Page(login)])
 
 pg.run()
-
-st.sidebar.write(f"Logged as {st.session_state.role}")

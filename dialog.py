84f5f61c9887efcb5f07c7c724aef0fb10b11511
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-22 15:01:31 +0200

    test

--- dialog.py
+++ dialog.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import datetime
 
 if "vote" not in st.session_state:
     st.session_state.vote=None
@@ -6,8 +7,8 @@ if "vote" not in st.session_state:
 @st.dialog("Choisissez")
 def vote():
     in_name = st.text_input("Nom de l'élève")
-    in_date = st.text_input("Date")
-    in_heure = st.text_input("Heure")
+    in_date = st.datetime_input("Date")
+    
 
 
     col1, col2, col3 = st.columns(3)
@@ -16,8 +17,7 @@ def vote():
         if st.button("Ok"):
             st.session_state.vote=("A voté")
             st.session_state.vote = {"Nom de l'élève": in_name, 
-                                    "Date": in_date, 
-                                    "Heure": in_heure}
+                                    "Date": in_date,}
             st.rerun()
 
     with col3:
@@ -36,6 +36,13 @@ if st.session_state.vote is not None:
 if st.button("Sélectionner un cours"):
     vote()
     
+
+   # "Choisissez la date et l'heure",
+    #datetime.datetime(2025, 11, 19, 16, 45),
+
+#st.write("Choisi", event_time)
+
+
 
 #else:
 #   f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"    

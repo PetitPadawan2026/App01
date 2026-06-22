cef0d2ee34b9d3657d73e5feaea5a953476fe9ed
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-22 15:10:46 +0200

    test

--- dialog.py
+++ dialog.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import datetime
 
-if "vote" not in st.session_state:
-    st.session_state.vote=None
+if "book_event" not in st.session_state:
+    st.session_state.book_event=None
 
 @st.dialog("Choisissez")
-def vote():
+def book_event():
     in_name = st.text_input("Nom de l'élève")
     in_date = st.datetime_input("Date")
     
@@ -15,26 +15,26 @@ def vote():
 
     with col1:
         if st.button("Ok"):
-            st.session_state.vote=("A voté")
-            st.session_state.vote = {"Nom de l'élève": in_name, 
+            st.session_state.book_event=("A voté")
+            st.session_state.book_event = {"Nom de l'élève": in_name, 
                                     "Date": in_date,}
             st.rerun()
 
     with col3:
         if st.button("Annuler"):
-            st.session_state.vote=None
+            st.session_state.book_event=None
             st.rerun()
 
     #if st.button("Submit"):
-        #st.session_state.vote = {"item": item, "reason": reason}
+        #st.session_state.book_event = {"item": item, "reason": reason}
         
 
-if st.session_state.vote is not None:
+if st.session_state.book_event is not None:
     st.write("Choix:")
-    st.dataframe(st.session_state.vote)
+    st.dataframe(st.session_state.book_event)
 
 if st.button("Sélectionner un cours"):
-    vote()
+    book_event()
     
 
    # "Choisissez la date et l'heure",
@@ -42,7 +42,7 @@ if st.button("Sélectionner un cours"):
 
 #st.write("Choisi", event_time)
 
-
+#{"allDay":false,"title":"Event 1","start":"2026-06-16T08:30:00+02:00","end":"2026-06-16T10:30:00+02:00","resourceId":"a"}
 
 #else:
 #   f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"    

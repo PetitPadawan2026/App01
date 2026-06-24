277c905aee703bd13d62a3a0e9f3fa85223bd39c
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-24 16:09:45 +0200

    test

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ import datetime
 test=False
 notes=["1","2","3","4","5"]
 
-
 def build_slider(label="Demo",values=[]):
   return st.select_slider(
     label,
     options=values,
   )
-
 
 def page_1():
  st.header("Page 1")
@@ -23,23 +21,20 @@ def page_menu():
 
 #pg = st.navigation([page_1, page_2])
 #pg.run()
-
 pages ={
  "Menu": [
   st.Page(page_menu, title="Menu", icon="🏠"),
  ],
  "Inscription / Connexion": [
-  st.Page("inc_streamlit_app.py", title="**Login**", icon="📌"),
+  st.Page("inc_streamlit_app.py", title="**Login**", icon="🔐"),
   st.Page("form.py", title="Formulaires", icon="📋"),
  ],
 "Réservation":[
-  st.Page("calendar.py", title="Calendrier", icon="📋"),
+  st.Page("calendar.py", title="Calendrier", icon="📅"),
   st.Page("dialog.py", title="Dialogue", icon="📋"),
-  st.Page("resa.py", title="Reservation", icon="📋"),
-
+  st.Page("resa.py", title="Reservation", icon="📝"),
  ] 
 }
-
 
 pg = st.navigation(pages)
 pg.run()

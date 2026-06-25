d9426b02ada63f464ddab2ca83a27899d2660879
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-25 16:32:37 +0200

    updated_events

--- calendar.py
+++ calendar.py
@@ -46,7 +46,7 @@ calendar_events = [
 if "updated_events" not in st.session_state:
     st.session_state.updated_events=calendar_events
 else:
-    calendar_events=st.session_state.updated_events
+    calendar_events=pd.DataFrame(st.session_state.updated_events)
 st.write(st.session_state.updated_events)
 
 if "calendar_events" not in st.session_state:

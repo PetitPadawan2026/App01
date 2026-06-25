746269eda3fbb1e02f79b12b76d5c6bd43987f73
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-25 10:50:35 +0200

    test

--- calendar.py
+++ calendar.py
@@ -167,7 +167,7 @@ def charger_excel():
         st.write(df_niv['niveau_txt'][ sel_niveau ])
 
     df=pd.read_excel(
-        new_df_cours = df['cours'].dropna('')
+        new_df_cours = df_cours.dropna('')
      )
 
 #sel_niveau = st.selectbox("Niveau:", 

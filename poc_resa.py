bad3c6df5dcd1e1c06fa42c0df1d2e0948549ed2
PetitPadawan2026 <jean-toussaint@outlook.com>
2026-06-26 10:01:38 +0200

    test

--- poc_resa.py
+++ poc_resa.py
@@ -269,7 +269,7 @@ def charger_excel():
 def make_select_niveau(txt_label="Test"):
     return st.selectbox(
         txt_label,
-        ("Niveau Débutant", "Niveau Confirmé", "Niveau Expert"),
+        ("Tous Niveaux ", "Niveau 1 ", "Niveau 2 ", "Niveau 3 ", "Niveau 4 ", "Niveau 5 ", "Niveau 6 ", "Niveau 7 ", "Niveau 8 ", "Niveau 9 ", "Niveau 10 ", "Niveau 11 ", "Niveau 12 "),
         label_visibility="hidden" if txt_label == "Test" else "visible"
     )
 
